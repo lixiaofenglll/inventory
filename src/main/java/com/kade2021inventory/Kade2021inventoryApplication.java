bd554d1f396f0c8f9d6baa0fb27d8b2124e8019b
lxf <1002777601@qq.com>
2021-04-08 10:43:06 +0800

    lxf

--- src/main/java/com/kade2021inventory/Kade2021inventoryApplication.java
+++ src/main/java/com/kade2021inventory/Kade2021inventoryApplication.java
@@ -1,33 +1,10 @@
-package com.kade2021Inventory;
+package com.kade2021inventory;
 
-import com.kade2021inventory.system.mapper.OmniMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+/**
+ * @Auther: lixiaofeng
+ * @Date:2021/4/8 10:19
+ * @Description:
+ * @version：1.0
+ **/
 public class kade2021inventoryApplication {
-    @Autowired
-    OmniMapper omniMapper;
-
-    public static void main(String[] args) {
-
-        SpringApplication.run(kade2021inventoryApplication.class, args);
-       /*OmniMapper o = run.getBean("omniMapper",OmniMapper.class);
-        HashMap<String ,Object> hm = new HashMap<>();
-        hm.put("_table","bug_detail,bug_project");
-        HashMap<String ,Object> arg = new HashMap<>();
-        arg.put("bug_project.id","bug_detail.projectId");
-        hm.put("connect",arg);
-        List<Map<String,Object>> re= o.selectAll(hm);
-        for (Map<String ,Object> map: re
-             ) {
-            for (Map.Entry<String ,Object> e:map.entrySet()
-                 ) {
-                System.out.print(e.getKey()+":"+e.getValue()+" ");
-            }
-            System.out.println();
-        }*/
-    }
-
 }
